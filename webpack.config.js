3b06c30d01dd525b6ab5404cdba60cfd2537e8c4
li-yq <39945542+li-yq@users.noreply.github.com>
2025-05-18 17:24:26 +0800

    simplify debug settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,11 @@
 /* eslint-disable no-undef */
 
-const devCerts = require("office-addin-dev-certs");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 
 const urlDev = "https://localhost:3000/";
 const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
-
-async function getHttpsOptions() {
-  const httpsOptions = await devCerts.getHttpsServerOptions();
-  return { ca: httpsOptions.ca, key: httpsOptions.key, cert: httpsOptions.cert };
-}
 
 module.exports = async (env, options) => {
   const dev = options.mode === "development";
@@ -101,7 +95,7 @@ module.exports = async (env, options) => {
       },
       server: {
         type: "https",
-        options: env.WEBPACK_BUILD || options.https !== undefined ? options.https : await getHttpsOptions(),
+        options: env.WEBPACK_BUILD,
       },
       port: process.env.npm_package_config_dev_server_port || 3000,
     },
